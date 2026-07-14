a9e8e29c17898650a9636ba32f3cdbec2a03c48b
gururajbhagav <gururajbhagav555@gmail.com>
2026-07-15 01:38:03 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MenuItemListCreateView, BookingListCreateView
-
-urlpatterns = [
-    path('menu-items/', MenuItemListCreateView.as_view(), name='menu-items'),
-    path('bookings/', BookingListCreateView.as_view(), name='bookings'),
-]
